96043f34c2595578007c72488fa2b4938a6b76a5
Leo Ermebring <leo.ermebring@edu.lycksele.se>
2025-01-27 13:00:06 +0100

    first commit

---
+++ Chuck norris/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+
+class Program{
+  static async void Main(){
+  using(HttpClient tysken = new HttpClient()){
+    tysken.BaseAddress = new Uri ("https://api.chucknorris.io");
+    try{
+HttpResponseMessage response = await tysken.GetAsync("jokes/random");
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+Console.WriteLine(responseBody);
+}
+catch(HttpRequestException e){
+  Console.WriteLine(e.Message);
+}
+}
+}
+}
